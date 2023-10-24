7651c263d66b3fbcf2df3355b09e46a7c19cb6bc
조익 <dream-ik89@naver.com>
2023-10-24 15:27:31 +0900

    feat: 엔티티 처리 관련 주의사항 적용
    - LAZY
    - casecade.ALL
    - 연관관계 편입 메서드

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -14,14 +14,14 @@ public class Order {
     private Long id;
 
 //양방향 연관관계(member-orders)인 경우 FK가 있는 곳을 연관관계의 주인으로 잡는다.
-    @ManyToOne //다대 일 관계
+    @ManyToOne(fetch = FetchType.LAZY) //다대 일 관계
     @JoinColumn(name = "member_id")//FK
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)//casecade All : order를 저장하면 orderItems도 저장
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne //1:1인경우 조회를 자주하는 객체에서 FK를 지정
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) //1:1인경우 조회를 자주하는 객체에서 FK를 지정
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -30,4 +30,17 @@ public class Order {
     @Enumerated(EnumType.STRING) //default가 ORDINAL임. 숫자형 외의 다른 값이 들어가면 에러
     private OrderStatus status; //주문상태 ORDER, CANCEL
 
+    //==연관관계 메서드==//
+    public void setMember(Member member){
+        this.member = member; //일반적인 setter
+        member.getOrders().add(this); // mapped 객체에 추가
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);//list인경우
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;//양방향
+        delivery.setOrder(this);
+    }
 }
