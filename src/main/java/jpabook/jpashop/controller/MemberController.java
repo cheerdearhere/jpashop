68333d40910945b0be965a3a1e14afe96c04abbd
조익 <dream-ik89@naver.com>
2023-10-31 09:59:22 +0900

    feat: 회원 추가 로직
    - memberForm을 사용해 member 정보를 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,14 +1,17 @@
 package jpabook.jpashop.controller;
 
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.dto.param.MemberForm;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -21,11 +24,20 @@ public class MemberController {
         return "members/createMemberForm";
     }
     @PostMapping("/members/new")
-    public String addUser(Model model, @RequestBody MemberForm memberForm){
+    public String addUser(@Valid MemberForm memberForm, BindingResult result){
+
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(),memberForm.getStreet(),memberForm.getZipcode());
+
         Member member = new Member();
         member.setName(memberForm.getName());
+        member.setAddress(address);
+
         memberService.join(member);
 
-        return "/";
+        return "redirect:/";
     }
 }
